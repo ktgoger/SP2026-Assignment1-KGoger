bce8a20be1a872f069913184bcc0d1c7dbe22018
ktgoger <ktgoger@crimson.ua.edu>
2026-01-27 21:58:02 -0600

    Add project files.

---
+++ SP2026-Assignment1-KGoger/Program.cs
@@ -0,0 +1,16 @@
+namespace SP2026_Assignment1_KGoger
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.Run();
+        }
+    }
+}
